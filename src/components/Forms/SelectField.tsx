27b764be2d2acb8b8701bcd8c72b237b6413d9c7
parvez13913 <parvezz13913@gmail.com>
2024-08-29 17:23:59 +0600

    [DONE] Select field component

---
+++ src/components/Forms/SelectField.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Select } from "antd";
+import { useFormContext, Controller } from "react-hook-form";
+
+export type SelectOptions = {
+  value: string;
+  label: string;
+};
+type SelectFieldProps = {
+  options: SelectOptions[];
+  name: string;
+  size?: "large" | "small";
+  value?: string | string[] | undefined;
+  placeholder?: string;
+  label?: string;
+  defaultValue?: SelectOptions;
+  handleChange?: (el: string) => void;
+};
+
+const SelectField = ({
+  name,
+  size = "large",
+  value,
+  placeholder = "select",
+  options,
+  label,
+  defaultValue,
+  handleChange,
+}: SelectFieldProps) => {
+  const { control } = useFormContext();
+
+  return (
+    <>
+      {label ? label : null}
+      <Controller
+        control={control}
+        name={name}
+        render={({ field: { value, onChange } }) => (
+          <Select
+            onChange={handleChange ? handleChange : onChange}
+            size={size}
+            options={options}
+            value={value}
+            placeholder={placeholder}
+            style={{
+              width: "100%",
+            }}
+          />
+        )}
+      />
+    </>
+  );
+};
+
+export default SelectField;
