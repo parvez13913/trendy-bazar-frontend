19b2371230a82e4960fe950de09aea20d624ee20
parvez <parvezz13913@gmail.com>
2024-12-12 19:08:28 +0600

    [DONE] All setup

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,18 +1,10 @@
 "use client";
-import { Row } from "antd";
 
 const ErrorPage = () => {
   return (
-    <Row
-      justify="center"
-      align="middle"
-      style={{
-        height: "100vh",
-        color: "red",
-      }}
-    >
+    <div>
       <h1>Something went wrong!</h1>
-    </Row>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Kichu michu</h1>
+      <h1 className="text-center">Kichu michu</h1>
     </div>
   );
 }
